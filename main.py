3a6fb7d5157ad215ef5e927e33867bb5156ac553
Mahfuz9999 <114106379+Mahfuz9999@users.noreply.github.com>
2023-11-23 15:29:59 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+todos = []
+print(type(todos))
+
+
+while True:
+    user_action = input("Type add or show :")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Type new todo : ")
+            todos.append(todo)
+        case 'show':
+            for item in todos:
+                item = item.title()
+                print(item)
+
+        case 'edit':
+            number = int(input("Enter the number of todo"))
+            number = number - 1
+            todos_edit = todos[number]
+            print (todos_edit)
+
+        case 'exit':
+            break
+
+print("Bye")
+# for item in ["sandals", "glasses", "trousers"]:
+#     print(item.capitalize())
