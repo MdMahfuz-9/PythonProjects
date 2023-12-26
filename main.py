40b91b97a5b368402719071be59ea28cd839b1b0
Mahfuz9999 <mahfuzfahadofficial@gmail.com>
2023-12-26 19:58:19 +0800

    22/12/2023

--- main.py
+++ main.py
@@ -6,25 +6,33 @@ while True:
         case 'add':
             todo = input("Enter a todos :-") + ("\n")
 
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            # file = open('todos.txt', 'r') -- Previous 1
+            # todos = file.readlines()
+            # file.close()
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with  open('todos.txt', 'w') as file:
+                file.writelines(todos)
         case 'show':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            # file = open('todos.txt', 'r')
+            # todos = file.readlines()
+            # file.close()
 
-            new_todos = []
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
-            for item in todos:
-                new_item = item.strip('\n')
-                new_todos.append(new_item)
+            # new_todos = []
+
+            # for item in todos:
+            #     new_item = item.strip('\n')
+            #     new_todos.append(new_item)
+
+            #shorter way of for-loop
+            new_todos = [item.strip('\n') for item in todos]
 
             for index, item in enumerate(new_todos):
                 row = f"{index}-{item}"
@@ -36,12 +44,19 @@ while True:
             pop_num = int(input("which one did you complete:-"))
             todos.pop(pop_num)
         case 'edit':
-            for item in todos:
-                print(item)
+            with open('todos.txt', 'r') as file: #read files
+                todos = file.readlines()
+
+            for index, item in enumerate(todos): # print with indexing
+                row = f"{index}-{item}"
+                print(row)
             edit_num = int(input("which one do you like to edit:-"))
 
-            replaced_todos = input("New todos :- ")
-            todos[edit_num] = replaced_todos
+            replaced_todos = input("New todos :- ") # replace todos
+            todos[edit_num] = replaced_todos + '\n'
+
+            with  open('todos.txt', 'w') as file: # save the new todos to the file
+                file.writelines(todos)
         case 'exit':
             break
 
