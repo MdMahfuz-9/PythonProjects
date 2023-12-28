2f6f2113e1c521b8138331dcbb3971f83e9a3c5c
Mahfuz9999 <mahfuzfahadofficial@gmail.com>
2023-12-29 00:36:19 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.py

--- Todos_App/main.py
+++ Todos_App/main.py
@@ -3,17 +3,17 @@ while True:
     user_action = user_action.strip()
 
 
-    if 'add' in user_action or 'new' in user_action:
+    if user_action.startswith('add'):
         todo = user_action[4:] + '\n'
 
         with open('todos.txt', 'r') as file: #read files
             todos = file.readlines()
 
-        todos.append(todo)
+        todos.append(todo )
 
         with open('todos.txt', 'w') as file:  # save the new todos to the file
             file.writelines(todos)
-    elif 'show' in user_action:
+    elif user_action.startswith('show'):
 
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
@@ -24,14 +24,18 @@ while True:
             row = f"{index}-{item}"
             print(row)
 
+
     elif 'edit' in user_action:
+
         with open('todos.txt', 'r') as file: #read files
             todos = file.readlines()
 
-        for index, item in enumerate(todos): # print with indexing
+        new_todos = [item.strip('\n') for item in todos]
+
+        for index, item in enumerate(new_todos):
             row = f"{index}-{item}"
             print(row)
-        edit_num = int(user_action[5:])
+        edit_num = int(input("Enter number -"))
 
         replaced_todos = input("enter new todo")
         todos[edit_num] = replaced_todos + '\n'
@@ -39,21 +43,24 @@ while True:
         with  open('todos.txt', 'w') as file: # save the new todos to the file
             file.writelines(todos)
 
-    elif 'complete' in user_action:
+    elif user_action.startswith('complete'):
+        try:
+            with open('todos.txt', 'r') as file: #read files
+                todos = file.readlines()
 
-        with open('todos.txt', 'r') as file: #read files
-            todos = file.readlines()
+            for index, item in enumerate(todos):
+                row = f"{index}-{item}"
+                print(row)
+            pop_num = int(input("which one is complete:- "))
+            todos.pop(pop_num)
 
-        for index, item in enumerate(todos):
-            row = f"{index}-{item}"
-            print(row)
-        pop_num = int(input("which one is complete:- "))
-        todos.pop(pop_num)
+            with  open('todos.txt', 'w') as file: # save the new todos to the file
+                file.writelines(todos)
+        except IndexError:
+            print("No item with that number.")
+            continue
 
-        with  open('todos.txt', 'w') as file: # save the new todos to the file
-            file.writelines(todos)
-
-    elif 'exit' in user_action:
+    elif user_action.startswith('exit'):
         break
     else:
         print("Command not found")
